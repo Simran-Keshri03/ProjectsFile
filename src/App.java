a72606cebe238543b675fcd994cb309a3be64551
Simran-Keshri03 <simrangrd373@gmail.com>
2026-07-09 11:29:51 +0530

    commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        greet();
+    }
+    public static void greet(){
+        System.out.println("hii there is an apple ");
+    }
+}
